c05e44f8b2447aca1e7ada85915d5ecd1f3d92fa
pratikstemkar <pratikstemkar@gmail.com>
2023-07-02 12:34:00 +0530

    bug fixes add role

--- internal/database/user.go
+++ internal/database/user.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"errors"
-	"fmt"
 
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/pratikstemkar/matchup/internal/models"
@@ -19,7 +18,7 @@ func CreateUser(user *models.User) (*models.User, error) {
 		user.Roles = append(user.Roles, role)
 		// fmt.Print(role)
 	}
-	fmt.Print(user)
+	// fmt.Print(user)
 	res := DB.Create(user)
 	if res.RowsAffected == 0 {
 		return &models.User{}, errors.New("user not created")
@@ -29,7 +28,7 @@ func CreateUser(user *models.User) (*models.User, error) {
 
 func ReadUser(user_id string) (*models.User, error) {
 	var user models.User
-	res := DB.First(&user, "user_id = ?", user_id)
+	res := DB.Preload("Roles").Find(&user, "user_id = ?", user_id)
 	if res.RowsAffected == 0 {
 		return nil, errors.New("user not found")
 	}
@@ -38,7 +37,8 @@ func ReadUser(user_id string) (*models.User, error) {
 
 func ReadUserList() ([]*models.User, error) {
 	var userList []*models.User
-	res := DB.Find(&userList)
+	// res := DB.Find(&userList)
+	res := DB.Model(&models.User{}).Preload("Roles").Find(&userList)
 	if res.Error != nil {
 		return nil, errors.New("user list not found")
 	}
@@ -47,6 +47,16 @@ func ReadUserList() ([]*models.User, error) {
 
 func UpdateUser(user *models.User) (*models.User, error) {
 	var updateUser models.User
+	// fmt.Println("User roles:")
+	roles_copy := user.Roles
+	user.Roles = []models.Role{}
+	for _, user_role := range roles_copy {
+		// fmt.Println(user_role.Rolename)
+		var role models.Role
+		DB.FirstOrCreate(&role, user_role)
+		user.Roles = append(user.Roles, role)
+		// fmt.Print(role)
+	}
 	res := DB.Model(&updateUser).Where("user_id = ?", user.User_Id).Updates(user)
 	if res.RowsAffected == 0 {
 		return &models.User{}, errors.New("user not updated")
