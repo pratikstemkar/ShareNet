7c0539d5c425a66980cdcba65835499bbd26b416
pratikstemkar <pratikstemkar@gmail.com>
2023-07-03 10:28:46 +0530

    nextconfig fix

--- web/next.config.js
+++ web/next.config.js
@@ -1,16 +1,10 @@
-/** @type {import('next').NextConfig} **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
 				protocol: "https",
 				hostname: "i.kinja-img.com",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "e0.pxfuel.com",
 				port: "",
 				pathname: "/**",
 			},
